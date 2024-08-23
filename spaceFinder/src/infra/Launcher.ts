065012d76888e4f059769d1d882f2c8f59b7fad4
shikhacse <shikhacse2010@gmail.com>
2024-08-22 22:07:36 -0500

    Initial commit

---
+++ spaceFinder/src/infra/Launcher.ts
@@ -0,0 +1,17 @@
+import { App } from "aws-cdk-lib";
+import { ApiStack } from "./stacks/ApiStack";
+import { DataStack } from "./stacks/DataStacks";
+import { LambdaStack } from "./stacks/LambdaStack";
+
+
+
+const app = new App();
+const dataStack = new DataStack(app, 'DataStack');
+const lambdaStack = new LambdaStack(app, 'LambdaStack', {
+    spacesTable : dataStack.spacesTable
+});
+new ApiStack(app, 'ApiStack',
+    {
+    helloLambdaIntegration : lambdaStack.helloLambdaIntegration
+    } 
+)
